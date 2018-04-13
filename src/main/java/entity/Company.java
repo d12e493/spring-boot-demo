e051bd12fca7ada14d7a660cb08357d1a0552b07
Davis Chen <Davis@Davis-Macbook-Pro-13.local>
2018-04-13 14:33:09 +0800

    init commit

---
+++ src/main/java/entity/Company.java
@@ -0,0 +1,17 @@
+package entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Company {
+    @Id
+    @GeneratedValue(generator = "companyGenerator")
+    @GenericGenerator(name = "companyGenerator", strategy = "increment")
+    private Long id;
+
+    @Column
+    private String name;
+}
